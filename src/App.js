c4f01886b87d7ce4eb3e77c821f7693960995a1c
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-04-20 11:13:18 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  //added code for event listener
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'INCREASE_COUNT'
+    });
+  }
+  //
+
   render() {
     return (
       <div className="App">
+        <button onClick={(event) => this.handleOnClick(event)}>Click</button>
+        <p>{this.props.store.getState().items.length}</p>
         App component
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; /* when invoked, return an instance of Redux store. We can use method to create store */
+import {Provider} from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
 
+/* pass down newly created store to our app Component as a prop. Reducer is in reducer file in src */
+const store = createStore(shoppingListItemReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); /* call create store method where we pass reducer */
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>
+  ,
   document.getElementById('root')
 );
